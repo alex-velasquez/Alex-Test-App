79fea4dd8287553bcc61dc5a0f15e46ab09cd547
Velasquez, Alex <alex.velasquez@cuc.org>
2019-05-03 09:32:10 -0500

    testing commit process

--- testapp/Program.cs
+++ testapp/Program.cs
@@ -20,6 +20,7 @@ namespace testapp
        //this is a comment 
 
             //comment for release branch
+            //this is another test to see a commit go through
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
